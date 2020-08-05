423c961a8364c45df36679d2404663291dcf726b
Yanaheng <1677360167@qq.com>
2020-08-05 23:09:03 +0800

    添加待完成菜单

--- config/route.config.ts
+++ config/route.config.ts
@@ -36,8 +36,36 @@ const routes = [
             component: '@/pages/plugins/reactAmap/index',
             menu: {
               name: '高德地图',
-            }
-          }
+            },
+          },
+          {
+            path: '/plugins/splitPane',
+            component: '@/pages/plugins/splitPane/index',
+            menu: {
+              name: 'splitPane',
+            },
+          },
+          {
+            path: '/plugins/markDown',
+            component: '@/pages/plugins/markDown/index',
+            menu: {
+              name: 'markDown',
+            },
+          },
+          {
+            path: '/plugins/jsonEditor',
+            component: '@/pages/plugins/jsonEditor/index',
+            menu: {
+              name: 'jsonEditor',
+            },
+          },
+          {
+            path: '/plugins/excels',
+            component: '@/pages/plugins/excels/index',
+            menu: {
+              name: 'excels',
+            },
+          },
         ]
       },
     ],
