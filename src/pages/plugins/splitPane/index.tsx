7291fd529099752cc3df29a5834ff9edaa479eed
panpan <18330108983@163.com>
2020-08-13 17:43:22 +0800

    splitPane first commit

--- src/pages/plugins/splitPane/index.tsx
+++ src/pages/plugins/splitPane/index.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { Card } from 'antd';
+import SplitPane from 'react-split-pane';
+import styles from './index.less';
 
 export default () => {
   return (
-    <div>
-      'splitPane'
-    </div>
-  )
-}
+    <Card
+      className={styles.container}
+      bodyStyle={{ height: '100%', padding: 0 }}
+    >
+      <SplitPane split="vertical" minSize={200}>
+        <div className={styles.leftPart} />
+        <SplitPane split="horizontal" minSize={200}>
+          <div className={styles.rightTopPart}>试着拖拽一下分界线吧～</div>
+          <div className={styles.rightBottomPart} />
+        </SplitPane>
+      </SplitPane>
+    </Card>
+  );
+};
